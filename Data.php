92e21532a3f129cb9551c6e75aaedd3faaae8a14
Nofrets Poai <opetstudio@gmail.com>
2019-11-27 22:43:24 +0700

    init

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'password'; 
+$DatabaseName = 'siapdev_opetstudio_com'; 
+$DatabasePort = '3306'; 
+?>
